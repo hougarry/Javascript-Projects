b67cd14341b959b1953d6ce23e91e53039d5bcd6
Helena Hall <hougarry@gmail.com>
2023-10-15 23:07:05 +0800

    Update script.js

--- Gmail-contact/script.js
+++ Gmail-contact/script.js
@@ -1,30 +1,23 @@
-<script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
-// Initialize emailjs outside of the sendMail function
-emailjs.init("yuCcGjmpcWOZcdfxJ"); // Use your actual public key
-    
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Preventing default form submission
-    sendMail();
-});
+//init emailjs
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('yuCcGjmpcWOZcdfxJ');
+})();
 
-function sendMail(){
-    var params = {
-        sendername: document.querySelector("#sendername").value,
-        to: document.querySelector("#to").value,
-        subject: document.querySelector("#subject").value,
-        replyto: document.querySelector("#replyto").value,
-        message: document.querySelector("#message").value,
-    };
-
-    var serviceID = "service_qloxods"; // Your Service ID
-    var templateID = "template_zoke8au"; // Your Template ID
-
-    emailjs.send(serviceID, templateID, params)
-    .then( res => {
-        alert("Email successfully sent!")
-    })
-    .catch(err => {
-        console.error("Error sending email: ", err);
-        alert("Failed to send email. Please try again later.");
+//send email function
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('service_qloxods', 'template_wpl9rz7', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                alert('Email sent successfully!');
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert('Failed to send email. See the console for more details.');
+            });
     });
 }
+
+ 
